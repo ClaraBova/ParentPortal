4530347263f85ed678ed2710174a8bce0145df59
Clara Bova <email": null>
2016-09-16 10:18:14 -0700

    update user registration

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -10,7 +10,20 @@ end
 
 #create
 post '/register' do
-  @user = User.new(params[:user])
+  @locations = Location.all
+  user_params = params[:user]
+  if user_params[:location] == ""
+    status 422
+    @errors = ["Please select your location from the drop down list."]
+    erb :'/users/new'
+  else
+    city = user_params[:location].split(",")[0]
+    state = user_params[:location].split(",")[1].strip
+    user_location = Location.find_by_city_and_state(city, state)
+  end
+
+  @user = User.new(username: user_params[:username], email: user_params[:email], password: user_params[:password], user_type: user_params[:user_type], location: user_location)
+
   if @user.save
     session[:username]
     redirect :"/profile/#{@user.username}"
